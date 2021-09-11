45efd0ed856be820ad0369a86bc37dd655ad384c
Felipe Martins Frudeli <felipem.frudeli.dev@gmail.com>
2021-09-11 20:48:07 -0300

    Add new component 'Footer'

--- pages/Login.js
+++ pages/Login.js
@@ -1,5 +1,7 @@
 
 import HeadComponent from './components/Head';
+import Footer from './components/Footer';
+
 import styles from '../styles/login.module.css';
 
 export default function Home() {
@@ -9,9 +11,30 @@ export default function Home() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>
       
-      <div className={styles.Container}></div>
+      <div className={styles.Container}>
       <HeadComponent/>
-        
+        <div className={styles.Center}>
+            <h1>Minha área</h1>
+            <p>Insira seu email e senha</p>
+          <form>
+            <div className={styles.EmailFrame}><div className={styles.icon}></div><input type="text" id="fname" name="fname" placeholder="Email"/></div>
+            <div className={styles.PasswordFrame}><div className={styles.icon}></div><input type="password" id="lname" name="lname" placeholder="Senha"/></div>
+            <button className={styles.LastPassword}>Esqueçeu a senha?</button>
+            <div className={styles.ButtonFrame}>
+              <button type="submit">Entrar</button>
+            </div>
+            <div className={styles.GoogleOption}>
+              <button type="submit">
+                <div className={styles.GoogleIcon}></div>
+                <p>Ou entrar com o google</p></button>
+            </div>
+            <div className={styles.CreateAccont}>
+              <p>Não possui uma conta?</p>
+              <button>Crie uma conta agora!</button></div>
+          </form>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,10 +1,39 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/Footer.module.css'
 
 export default function Home() {
     return (
     <div className={styles.footer}>
-
+      <div className={styles.CardList}>
+        <div className={styles.Card}>
+          <div className={styles.imgLogo}></div>
+        </div>
+        <div className={styles.Card}>
+          <h1>Sobre</h1>
+          <button>Novidades</button>
+          <button>Nossa Equipe</button>
+          <button>Quem Somos</button>
+        </div>
+        <div className={styles.Card}>
+          <h1>Suporte</h1>
+          <button>Central de Ajuda</button>
+          <button>Recuperação de Contas</button>
+        </div>
+        <div className={styles.CardContacts}>
+          <h1>Contatos</h1>
+          <div className={styles.box}>
+            <button><div className={styles.InstagramIcon}></div></button>
+            <button><div className={styles.YoutubeIcon}></div></button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles.bottom}>
+        <p>© 2021 Yume</p>
+        <button className={styles.bottom}>Termos de uso</button>
+        <button className={styles.bottom}>Política de privacidade</button>
+        <button className={styles.bottom}>Aviso de cookies</button>
+      </div>
     </div>
   )
 }
