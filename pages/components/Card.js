afc6bafb60830c21c332039182de2f459039af60
Felipe Martins Frudeli <felipem.frudeli.dev@gmail.com>
2021-09-12 21:18:14 -0300

    2.3.1

--- pages/components/Card.js
+++ pages/components/Card.js
@@ -4,7 +4,7 @@ export default function ComParametro(props) {
     return (
         <div>
            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
 <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;700&family=Poppins&display=swap" rel="stylesheet"/>
             <div className={style.Card}>
                 <div className={style.ImageBox}>
